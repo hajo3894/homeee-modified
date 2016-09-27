d3455a2402616bc89f7978fdf2bc8c80616efdbc
hajo3894 <daniel.hammerschmidt@unitybox.de>
2016-09-27 12:41:53 +0200

    Ordner "lang" hinzugefügt

---
+++ shop/lang/english/admin/product_filter.php
@@ -0,0 +1,64 @@
+<?php
+/*
+      PRODUKT FILTER
+      Create & Copyright (C) 2009 xt-shopservice.de
+      Sebastian Schramm & Daniel Siekiera
+*/
+ 
+define('HEADING_TITLE','Product Filter PRO v.1.3');
+define('CONTENT_NOTE','');
+
+define('TABLE_HEADING_NAVIGATION','Navigation:');
+define('TABLE_HEADING_INFOCENTER_TOPIC','Pages: ');
+define('TABLE_HEADING_INFOCENTER_TOPIC_OFF','Thereof offline: ');
+
+define('TABLE_HEADING_NAVIGATION_OVERVIEW','Overview');
+define('TABLE_HEADING_NAVIGATION_NEWCATEGORIE','New filter category');
+define('TABLE_HEADING_NEWCATEGORIE_STATUS','Status');
+define('TABLE_HEADING_NAVIGATION_NEWITEM','New criterion');
+define('TABLE_HEADING_NAVIGATION_STARTSITE','Working on HOME');
+
+define('TABLE_HEADING_EDIT_ALL','Edit all title');
+define('TABLE_CAT_EMPTY','There is no criterion or you not set status on &quot;online&quot;.');
+define('TABLE_NO_CAT','Create at first a criterion or set the status on &quot;online.&quot;');
+
+define('TABLE_HEADING_TITLE','Filter categories');
+define('TABLE_HEADING_DESC','Content');
+
+define('TABLE_HEADING_ACTION','Acion');
+define('TABLE_HEADING_ACTION_EDIT_0','---');
+define('TABLE_HEADING_ACTION_EDIT','Edit');
+define('TABLE_HEADING_ACTION_NEWITEM','New');
+define('TABLE_STATUS','Set status');
+define('TABLE_STATUS_CAT','Enter filter category');
+
+define('TABLE_FOOTER_STATUS','Status: ');	
+define('TABLE_FOOTER_STATUS_0','---');	
+define('TABLE_FOOTER_STATUS_1','Offline');	
+define('TABLE_FOOTER_STATUS_2','Online');	
+define('TABLE_FOOTER_STATUS_3','Delete');	
+
+define('UPDATE_ENTRY','Update?');	
+define('UPDATE_SAVE','Store');	
+
+// NEWCATEGORIE
+define('TABLE_HEADING_NEWCATEGORIE_NAME','Name');	
+define('TABLE_HEADING_NEWCATEGORIE_POSITION','Position');	
+
+// NEWITEM
+define('TABLE_HEADING_NEWITEM_NAME','Name of filter criterion');
+define('TABLE_HEADING_NEWITEM_TITLE','Titel of the box');
+define('TABLE_HEADING_NEWITEM_DESCRIPTION','Description');
+define('TABLE_HEADING_NEWITEM_IMAGE','Image');
+define('NEWITEM_UPLOAD_FAIL', 'Extension not allowed. Only png, jpg, jpeg and gif are allowed. Image not be uploaded.');
+
+define('TABLE_HEADING_NEWCATEGORIE_MULTI', 'Is a multiple selection possible?');
+define('TABLE_HEADING_NEWCATEGORIE_ATT_INDIVIDUAL', 'Need we an individual selection if we have attributes?');
+
+define('INSERT_CRITERION_NOT_ALL_LANG', 'You must set a value for every language');
+define('INSERT_FILTER_CATEGORY_NOT_ALL_LANG', 'You must set a value for every language');
+define('INSERT_FILTER_CATEGORY_SUCCESS', 'Edit Category successful');
+define('INSERT_FILTER_CRITERION_SUCCESS', 'Edit Criterion successful');
+  
+define('REQUIRED', 'required');
+?>
---
+++ shop/lang/german/admin/product_filter.php
@@ -0,0 +1,66 @@
+<?php
+/*
+      PRODUKT FILTER
+      Create & Copyright (C) 2009 xt-shopservice.de
+      Sebastian Schramm & Daniel Siekiera
+*/
+ 
+define('HEADING_TITLE','Produkt Filter PRO v.1.3');
+define('CONTENT_NOTE','');
+
+define('TABLE_HEADING_NAVIGATION','Navigation:');
+define('TABLE_HEADING_INFOCENTER_TOPIC','Seiten: ');
+define('TABLE_HEADING_INFOCENTER_TOPIC_OFF','davon Offline: ');
+
+define('TABLE_HEADING_NAVIGATION_OVERVIEW','Übersicht');
+define('TABLE_HEADING_NAVIGATION_NEWCATEGORIE','Neue Kategorie');
+define('TABLE_HEADING_NEWCATEGORIE_STATUS','Status');
+define('TABLE_HEADING_NAVIGATION_NEWITEM','Neues Kriterium');
+define('TABLE_HEADING_NAVIGATION_STARTSITE','Startseite bearbeiten');
+
+define('TABLE_HEADING_EDIT_ALL','Alle Titeln bearbeiten');
+define('TABLE_CAT_EMPTY','Es gibt noch kein Kriterium oder Sie haben noch keines auf &quot;Online&quot; gesetzt.');
+define('TABLE_NO_CAT','Erstellen Sie zu allererst eine Kategorie oder setzen Sie einen Status auf &quot;Online.&quot;');
+
+define('TABLE_HEADING_TITLE','Kategorien');
+define('TABLE_HEADING_DESC','Content');
+
+define('TABLE_HEADING_ACTION','Aktion');
+define('TABLE_HEADING_ACTION_EDIT_0','---');
+define('TABLE_HEADING_ACTION_EDIT','Bearbeiten');
+define('TABLE_HEADING_ACTION_NEWITEM','Neu');
+define('TABLE_STATUS','Status setzen');
+define('TABLE_STATUS_CAT','Kategorie eintragen');
+
+define('TABLE_FOOTER_STATUS','Status: ');	
+define('TABLE_FOOTER_STATUS_0','---');	
+define('TABLE_FOOTER_STATUS_1','Offline');	
+define('TABLE_FOOTER_STATUS_2','Online');	
+define('TABLE_FOOTER_STATUS_3','L&ouml;schen');	
+
+define('UPDATE_ENTRY','Aktualisieren?');	
+define('UPDATE_SAVE','Speichern');	
+
+// NEWCATEGORIE
+define('TABLE_HEADING_NEWCATEGORIE_NAME','Bezeichung');	
+define('TABLE_HEADING_NEWCATEGORIE_POSITION','Position');	
+
+// NEWITEM
+define('TABLE_HEADING_NEWITEM_NAME','Kriteriumsname');	
+define('TABLE_HEADING_NEWITEM_TITLE','Titel für die Box');	
+define('TABLE_HEADING_NEWITEM_DESCRIPTION','Beschreibung');
+define('TABLE_HEADING_NEWITEM_IMAGE','Bild');
+define('NEWITEM_UPLOAD_FAIL', 'Ung&uuml;ltige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt. Bild wurde nicht hochgeladen.');
+
+define('TABLE_HEADING_NEWCATEGORIE_MULTI', 'Ist eine Mehrfachauswahl der Kriterien m&ouml;glich?');
+define('TABLE_HEADING_NEWCATEGORIE_ATT_INDIVIDUAL', 'Soll bei Attributen jedes einzelne Attribut eine Auswahl bekommen?');
+
+define('INSERT_CRITERION_NOT_ALL_LANG', 'Es muss f&uuml;r jede aktivierte Sprache ein Name / Wert angegeben werden');
+define('INSERT_FILTER_CATEGORY_NOT_ALL_LANG', 'Es muss f&uuml;r jede aktivierte Sprache ein Name angegeben werden');
+define('INSERT_FILTER_CATEGORY_SUCCESS', 'Kategorie erfolgreich bearbeitet');
+define('INSERT_FILTER_CRITERION_SUCCESS', 'Kriterium erfolgreich bearbeitet');
+
+define('REQUIRED', 'Pflichtfeld');
+  
+ 
+?>
